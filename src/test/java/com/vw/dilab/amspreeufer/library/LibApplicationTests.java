25f0497301cb55629c33f1e25360431c7f6b90ba
DiLab <dilab@rb26-bumblebee.local>
2016-04-18 14:30:48 +0200

    initial halloWorld state of the Lib Project

---
+++ src/test/java/com/vw/dilab/amspreeufer/library/LibApplicationTests.java
@@ -0,0 +1,18 @@
+package com.vw.dilab.amspreeufer.library;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LibApplication.class)
+@WebAppConfiguration
+public class LibApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
